f475662bcdb3070391899c2636e3d19ed8257fc7
Maurits Kok <mwakok@gmail.com>
2020-11-02 22:21:43 +0100

    Add module to import and parse data

---
+++ mitc/import_xlsx.m
@@ -0,0 +1,76 @@
+function out = import_xlsx
+    % import_xlsx - Import and parse .xlsx files
+    %
+    % Syntax: [output] = import_xlsx(filename)
+    %
+    % Inputs:
+    %   filename : string
+    %       filename of the .xlsx file to be imported
+    %   varargin : string
+    %       
+    %
+    % Outputs:
+    %   Data : structure?
+    %       
+    % Example:
+    %
+    % License:
+    % 
+    % 
+    %------------- BEGIN CODE --------------
+    
+    
+    filename = '..\data\Case study.xlsx';
+    
+    dataDouble = xlsread(filename);
+    dataCell = readcell(filename);
+    
+    %--- Activities duration (optimistic, most likely, and pessimitic) 
+    out.durationActivities = remove_nan(dataDouble(:,3:5));
+    out.nActivities = size(out.durationActivities, 1);
+    
+    %--- Risk events duration (optimistic, most likely, and pessimitic)
+    out.riskEventsDuration = remove_nan(dataDouble(:,19:21));
+
+    %--- Probability of occurence of risk events
+    out.riskProbability = remove_nan(dataDouble(:,23));
+    out.nRisks = size(out.riskProbability,1);
+
+    %--- Duration mitigated by every mitigation measure (minimum, most likely, and maximum)
+    out.mitigatedDuration = remove_nan(dataDouble(:,9:11));
+    out.nMitigations = size(out.mitigatedDuration, 1);
+
+    %--- Cost of the mitigation measures
+    out.mitigationCost = remove_nan(dataDouble(:,13:15)); 
+    
+    %--- Relation matrix indicating upon which activity i each mitigation measure j intervenes
+    R_ij = zeros(out.nMitigations, out.nActivities); 
+    R_ij_col = dataCell(:,16); % load the cells related to relationships between mitigation measures and activities
+    R_ij = find_dependencies(R_ij, R_ij_col, out.nMitigations);  
+    out.R_ij = R_ij'; %inverse the matrix
+
+    %--- Relation matrix indicating which activity i each risk event e effects (delays) -->(E_ie)
+    E_ie = zeros(out.nRisks, out.nActivities); %create SxN matrix: relationships between risk events and activities (the matrix is to be inversed later)
+    E_ie_col = dataCell(:,22); % load the cells related to relationships between risk events and activities
+    E_ie = find_dependencies(E_ie, E_ie_col, out.nRisks);  
+    out.E_ie=E_ie'; %inverse the matrix
+    
+    function arrayOut = remove_nan(arrayIn)
+        arrayIn(~any(~isnan(arrayIn), 2),:) = [];
+        arrayOut = arrayIn;
+    end
+
+    function matrixOut = find_dependencies(matrixIn, relation, iterations)
+        for i = 1 : iterations
+            if relation{i} == 0 % If risk event does not not affect activity case, do nothing
+            elseif isa(relation{i},'double') == 1 % assign interdependency
+                matrixIn(i, relation{i}) = 1;
+            elseif isa(relation{i},'char') == 1
+                matrixIn(i, str2num(relation{i})) = 1;
+            end
+        end
+        matrixOut = matrixIn;
+    end
+end
+
+
