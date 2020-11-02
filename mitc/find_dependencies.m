8606963783e3d9ed42b8f1e0398ed6a0f405603c
Maurits Kok <mwakok@gmail.com>
2020-11-02 22:20:07 +0100

    Fixed bug to determine double and char

--- mitc/find_dependencies.m
+++ mitc/find_dependencies.m
@@ -9,12 +9,12 @@ function matrixOut = find_dependencies(matrixIn, relation, iterations)
 
     for i = 1 : iterations
             if relation{i} == 0 % If risk event does not not affect activity case, do nothing
-            elseif isdouble(relation{i}) % assign interdependency
+            elseif isa(relation{i},'double') == 1 % assign interdependency
                 matrixIn(i, relation{i}) = 1;
-            elseif ischar(relation{i})
+            elseif isa(relation{i},'char') == 1
                 matrixIn(i, str2num(relation{i})) = 1;
             end
-    matrixOut = matrixIn;        
-    end       
+    end
+    matrixOut = matrixIn;
     
 end
