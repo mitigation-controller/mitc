ec0722a199fc7f74470575121ef23b8fa1ad13e8
Maurits Kok <mwakok@gmail.com>
2020-11-03 10:58:37 +0100

    Add module to draw a random number from the Beta-Pert distribution

---
+++ mitc/draw_random_pert.m
@@ -0,0 +1,13 @@
+function [x]=draw_random_pert(a,m,b)
+    % draw_random_pert - Draw a random number from the Beta-Pert
+    % distribution
+    %
+    %
+    %
+
+    %mu=(a+4*m+b)/6; % mean value
+    %sd=(b-a)/2; %standard deviation
+    alpha = 1+4*(m-a)/(b-a);% First Beta parameter computed using the three points optimistic, most likely, and pessimistic values
+    beta = 1+4*(b-m)/(b-a);% Second Beta parameter computed using the three points optimistic, most likely, and pessimistic values
+    x = randraw('Beta',[a b alpha beta],1); %draw a random number according to the Beta-Pert distribution
+end
