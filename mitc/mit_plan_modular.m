7af95bd6795c5e0d4682a3a68cad2c0f8f1e846b
Maurits Kok <mwakok@gmail.com>
2020-11-03 16:05:48 +0100

    Add first modules

--- mitc/mit_plan_modular.m
+++ mitc/mit_plan_modular.m
@@ -1,17 +1,27 @@
-% Temporary file to test modular appraoch to mit_plan
+% Temporary file to test modular approach to mit_plan
+
+addpath('bin', 'plotting')
 
 %--- 1) Import project data
+filename = '..\data\Case study.xlsx';
+[dataDouble, dataCell] = import_project(filename);
 
 %--- 2) Parse data
+parsedData = parse_data(dataDouble, dataCell);
 
 %--- 3) Prepare data for simulation
+% find_critical_paths
 
 %--- 4) Pre-draw random numbers
 
 %--- 5) Run simulation
+% mitc_simulation
 
 %--- 6) Treat simulation results
 
+
 %--- 7) Generate plots
+% plot_network
+% 
 
 %--- 8) Save data and plots
