b4bba379f4b4523c63aadb1bead07e2fa6a91589
Maurits Kok <mwakok@gmail.com>
2020-11-03 15:44:43 +0100

    Initialize file to test modules

---
+++ mitc/mit_plan_modular.m
@@ -0,0 +1,17 @@
+% Temporary file to test modular appraoch to mit_plan
+
+%--- 1) Import project data
+
+%--- 2) Parse data
+
+%--- 3) Prepare data for simulation
+
+%--- 4) Pre-draw random numbers
+
+%--- 5) Run simulation
+
+%--- 6) Treat simulation results
+
+%--- 7) Generate plots
+
+%--- 8) Save data and plots
