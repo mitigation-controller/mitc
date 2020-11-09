142910a5fcf8e6af728ddb565dfd2f88c39a465b
Maurits Kok <mwakok@gmail.com>
2020-11-09 20:57:35 +0100

    Create function that generats a matrix of random numbers

---
+++ mitc/bin/draw_random_numbers.m
@@ -0,0 +1,41 @@
+function randomNumbers = draw_random_numbers(property, rows, columns)
+% DRAW_RANDOM
+%
+% Input: 
+%   rows : int
+%       number of rows of the matrix    
+%   columns : int
+%       number of rows of the matrix  
+%   property : array [1,3]
+%       3 values: optimisitic, most likely, and pessimitic expectation
+%
+% Outputs:
+%   randomNumbers : 2D matrix
+%       matrix with size [rows, columns] filled with random numbers from
+%       the Pert-Beta distribution if applicable
+%
+
+randomNumbers = zeros(rows, columns);
+
+for j = 1 : columns
+    for i = 1 : rows
+        if exist_uncertainty(property(i,:))
+            % Draw a random number from Pert-Beta distribution
+            randomNumbers(i,j) = round(rand_pert(property(i,1),...
+                                                 property(i,2),...
+                                                 property(i,3))); 
+        else
+            randomNumbers(i,j) = property(i,2);
+        end
+    end
+end
+end
+
+% Determine whether property has uncertainty
+function out = exist_uncertainty(input)
+    if input(3) - input(1) > 0
+        out = true;
+    else
+        out = false;
+    end
+end
