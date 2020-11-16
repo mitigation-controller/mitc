d2c29996de07eb9d6bafe0ccbb74fc92b1ded484
Maurits Kok <mwakok@gmail.com>
2020-11-16 17:48:04 +0100

    Change structure names

--- mitc/mit_plan_modular.m
+++ mitc/mit_plan_modular.m
@@ -4,38 +4,38 @@ addpath('bin', 'plotting', 'modules')
 
 
 %--- User input
-nsimulations=1000;
-T_pl=1466;
+Config.nsimulations=500;
+Config.T_pl=1466;
 
 %--- 1) Import project data
 filename = '..\data\Case study.xlsx';
 [dataDouble, dataCell] = import_project(filename);
 
 %--- 2) Parse data
-parsedData = parse_data(dataDouble, dataCell);
+Data = parse_data(dataDouble, dataCell);
 
 %--- 3) Prepare data for simulation
 
 % 3a) Generate matrix with all paths
-[parsedData.P_ki, A] = fill_path_matrix(parsedData.R_ii, parsedData.nActivities);
+[Data.P_ki, A] = fill_path_matrix(Data.R_ii, Data.nActivities);
 
 % 3b) Select critical paths to reduce the simulation time
-[parsedData.T_orig, parsedData.P_cr_0, parsedData.K] = ...
+[Data.T_orig, Data.P_cr_0, Data.K] = ...
                          select_critical_paths(...                         
-                            parsedData.P_ki,...
-                            parsedData.durationActivities,...
-                            parsedData.riskEventsDuration,...
-                            parsedData.E_ie);
+                            Data.P_ki,...
+                            Data.durationActivities,...
+                            Data.riskEventsDuration,...
+                            Data.E_ie);
 
 %--- 4) Run simulation
-collectData = mitc_simulation(parsedData, nsimulations, T_pl);
+Results = mitc_simulation(Data, Config.nsimulations, Config.T_pl);
 
 %--- 5) Treat simulation results
 
 
 %--- 6) Generate plots
 plot_network(A, dataCell);
-plot_freq_mitigation(collectData, parsedData, nsimulations);
+plot_freq_mitigation(Results, Data, Config.nsimulations);
 % 
 
 %--- 7) Save data and plots
