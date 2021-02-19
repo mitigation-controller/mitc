45f1ed04babc82967d9c03c785c2d43b03dd8097
Maurits Kok <m.w.a.kok@tudelft.nl>
2021-02-19 14:37:42 +0100

    Update tests parse_data

--- tests/test_parse_data.m
+++ tests/test_parse_data.m
@@ -8,14 +8,46 @@ classdef test_parse_data < matlab.unittest.TestCase
    end
    
    methods (Test)
-       
+              
        function testActivities(testCase, dataDouble, dataCell)
            Data = parse_data(dataDouble, dataCell);
-           expNumActivities = 38;
-           testCase.verifyEqual(Data.nActivities, expNumActivities,...
+           expNumAct = 38;
+           testCase.verifyEqual(Data.nActivities, expNumAct,...
                'Expected a different number of activities')           
        end
        
+       function testMitigation(testCase, dataDouble, dataCell)
+           Data = parse_data(dataDouble, dataCell);
+           expNumMit = 19;           
+           testCase.verifyEqual(Data.nMitigations, expNumMit,...
+               'Expected a different number of mitigation events')
+       end
+       
+       function testRisks(testCase, dataDouble, dataCell)
+           Data = parse_data(dataDouble, dataCell);
+           expNumRisks = 19;
+           testCase.verifyEqual(Data.nRisks, expNumRisks,...
+               'Expected a different number of risk events')
+       end
+       
+       function testRemoveNan(testCase, dataDouble, dataCell)
+           Data = parse_data(dataDouble, dataCell);
+           nNan = sum(isnan(Data.durationActivities(:)));
+           testCase.verifyEqual(nNan, 0,...
+               'Expected all nan values to be removed from durationActivities')
+           nNan = sum(isnan(Data.riskEventsDuration(:)));
+           testCase.verifyEqual(nNan, 0,...
+               'Expected all nan values to be removed from riskEventsDuration')
+           nNan = sum(isnan(Data.riskProbability(:)));
+           testCase.verifyEqual(nNan, 0,...
+               'Expected all nan values to be removed from riskProbability')
+           nNan = sum(isnan(Data.mitigatedDuration(:)));
+           testCase.verifyEqual(nNan, 0,...
+               'Expected all nan values to be removed from mitigatedDuration')
+           nNan = sum(isnan(Data.mitigationCost(:)));
+           testCase.verifyEqual(nNan, 0,...
+               'Expected all nan values to be removed from mitigationCost')
+       end
+       
    end
-    
 end
