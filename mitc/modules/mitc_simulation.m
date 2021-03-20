8f777fae9c5c5e309762e24995464c65ec695e80
Maurits Kok <m.w.a.kok@tudelft.nl>
2021-03-20 16:48:25 +0100

    Refactor code and fix variance calculation

--- mitc/modules/mitc_simulation.m
+++ mitc/modules/mitc_simulation.m
@@ -80,7 +80,8 @@ c_j_matrix = mitigation_cost(Data.nMitigations,...
                              m_j_matrix); 
                          
 d_i_noCorrelation_parameters = calculate_no_correlation(d_i_total_matrix,...
-                                d_i_correlation_matrix, Data.durationActivitiesNoCorrelation);
+                                d_i_correlation_matrix,...
+                                Data.durationActivitiesNoCorrelation);
 
 d_i_noCorrelation_matrix = draw_random_numbers(d_i_noCorrelation_parameters,...
                                                Data.nActivities,...
@@ -217,27 +218,6 @@ end
 end
 
 %% --- Helper functions
-
-function d_i_noCorrelation_parameters = calculate_no_correlation(d_i_total_matrix, d_i_correlation_matrix, d_i_noCorrelation_all)
-
-    % Calculate correlation statistics
-    mean_d_i_total_matrix = mean(transpose(d_i_total_matrix));
-    variance_d_i_total_matrix = var(transpose(d_i_total_matrix));
-    mean_d_i_correlation_matrix = mean(transpose(d_i_correlation_matrix));    
-    variance_d_i_correlation_matrix = var(transpose(d_i_correlation_matrix));    
-
-    % Calculate no-correlation statistics
-    mean_d_i_noCorrelation_matrix = mean_d_i_total_matrix - mean_d_i_correlation_matrix;
-    variance_d_i_noCorrelation_matrix = variance_d_i_total_matrix - variance_d_i_correlation_matrix;
-    std_d_i_noCorrelation_matrix = variance_d_i_noCorrelation_matrix .^ 0.5;
-    
-    % Collect no-correlation parameters
-    d_i_noCorrelation_parameters = [d_i_noCorrelation_all(:,1),...
-                                d_i_noCorrelation_all(:, 3),...
-                                transpose(mean_d_i_noCorrelation_matrix),...
-                                transpose(std_d_i_noCorrelation_matrix)];
-    
-end
 
 function c_j_matrix = mitigation_cost(nMitigations, nSimulations, m_j_all, c_j_all, m_j_matrix)
     
